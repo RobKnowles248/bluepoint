f2f886d0f29f9b6f96a715087a3ea3b26e90fd03
RobKnowles248 <robert@knowles-family.me.uk>
2021-10-03 14:29:25 +0000

    Added donations to admin

--- donate/admin.py
+++ donate/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Donation
 
-# Register your models here.
+
+class DonationAdmin(admin.ModelAdmin):
+    readonly_fields = (
+        'donation_number', 'date', 'donation_amount')
+
+    fields = (
+        'donation_number', 'full_name', 'email', 'date', 'donation_amount')
+
+    list_display = (
+        'donation_number', 'date', 'full_name', 'donation_amount'
+    )
+
+    ordering = ('-date',)
+
+
+admin.site.register(Donation)
