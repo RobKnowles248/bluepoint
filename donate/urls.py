a7d37bd01b859d8ff052b9633949b892b3324cce
RobKnowles248 <robert@knowles-family.me.uk>
2021-10-11 18:22:40 +0000

    Add donation number parameter to pay view

--- donate/urls.py
+++ donate/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.donate, name='donate'),
-    path('pay/', views.pay, name='pay'),
+    path('pay/<donation_number>', views.pay, name='pay'),
 ]
