ee96f5a17c5810cb80b7ec3ee8c145631d7f4ec6
RobKnowles248 <robert@knowles-family.me.uk>
2021-10-11 19:11:04 +0000

    Complete submit event of payment form

--- donate/static/donate/js/stripe_elements.js
+++ donate/static/donate/js/stripe_elements.js
@@ -3,11 +3,11 @@ Logic, payment flow and styles from the stripe docs
 https://stripe.com/docs/payments/accept-a-payment
 */
 // Retrieve public and client secret keys
-var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-var client_secret = $('#id_client_secret').text().slice(1, -1);
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#client_secret').text().slice(1, -1);
 
 // Create an instance of Elements
-var stripe = Stripe(stripe_public_key);
+var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 
 var style = {
@@ -23,11 +23,36 @@ var card = elements.create('card', {style: style });
 card.mount('#card-element');
 
 // Error events
-// card.on('change', ({error}) => {
-//     let displayError = document.getElementById('#card-errors');
-//     if (error) {
-//         displayError.textContent = error.message;
-//     } else {
-//         displayError.textContent = '';
-//     }
-// });
+let displayError = document.getElementById('#card-errors');
+card.on('change', ({error}) => {
+    if (error) {
+        displayError.textContent = error.message;
+    } else {
+        displayError.textContent = '';
+    }
+});
+
+// Submit payment to Stripe
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+    card.update({'disabled': true});
+    $('#submit-button').attr('disabled', true)
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+        if (result.error) {
+            displayError.textContent = result.error.message;
+            card.update({'disabled': false});
+            $('#submit-button').attr('disabled', false)
+        } else {
+            // displayError.textContent = '';
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+        }
+    });
+});
