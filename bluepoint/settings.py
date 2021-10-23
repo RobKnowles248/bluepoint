7ca811e2038731ac45e426a1db2024332a4c665a
RobKnowles248 <robert@knowles-family.me.uk>
2021-10-23 10:41:58 +0000

    Removed secret key and set debug

--- bluepoint/settings.py
+++ bluepoint/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9xq4y@jc@ct46u6*)mgehmcl)(x*7ou3p1wa6v+$msx8al$)p*'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['bluepoint-rob.herokuapp.com', 'localhost']
 
