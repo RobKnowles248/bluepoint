87668e3e789eb0ebef00d73b4082225c16d5552f
RobKnowles248 <robert@knowles-family.me.uk>
2021-10-24 15:26:57 +0000

    Retry build after changing config vars

--- bluepoint/settings.py
+++ bluepoint/settings.py
@@ -174,6 +174,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'bluepoint-rob'
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
