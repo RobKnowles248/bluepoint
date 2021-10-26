2a46a535ba1551007845ab55d6d3fcc245d06dd7
RobKnowles248 <robert@knowles-family.me.uk>
2021-10-26 18:10:47 +0000

    Add latest bluepoints to homepage

--- home/views.py
+++ home/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
+from logbooks.models import Bluepoint
 
 
 def index(request):
     """ A view to return the index page """
+    recent_bluepoints = Bluepoint.objects.all().order_by(
+        'date_added__day', 'date_added__hour', 'date_added__minute'
+    ).reverse()[:10]
 
-    return render(request, 'home/index.html')
+    template = 'home/index.html'
+    context = {
+        'recent_bluepoints': recent_bluepoints,
+    }
+
+    return render(request, template, context)
