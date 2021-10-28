4f7e1ae4a35b0d571ffc26e7a8b377fd9a11993d
RobKnowles248 <robert@knowles-family.me.uk>
2021-10-28 19:10:33 +0000

    Add top bluepoints functionality

--- home/views.py
+++ home/views.py
@@ -5,13 +5,20 @@ from logbooks.models import Bluepoint
 
 def index(request):
     """ A view to return the index page """
+    # Get the 10 most recent bluepoints
     recent_bluepoints = Bluepoint.objects.all().order_by(
         'date_added__day', 'date_added__hour', 'date_added__minute'
+    ).reverse()[:10]
+
+    # Get the 10 top bluepoints
+    top_bluepoints = Bluepoint.objects.all().order_by(
+        'sort_grade'
     ).reverse()[:10]
 
     template = 'home/index.html'
     context = {
         'recent_bluepoints': recent_bluepoints,
+        'top_bluepoints': top_bluepoints,
         'MEDIA_URL': settings.MEDIA_URL,
     }
 
